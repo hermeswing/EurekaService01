44abe57909c57af35fea6a29e599ef3e38073dfb
jypark-ubuntu <hermeswing@naver.com>
2024-03-28 13:28:00 +0900

    Eureka Service #01

---
+++ src/main/java/octopus/EurekaService01Application.java
@@ -0,0 +1,13 @@
+package octopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaService01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaService01Application.class, args);
+    }
+
+}
